9dc91625bd4373359a8624f3d93a51249d321fd2
bob <843024750@qq.com>
2017-05-16 19:39:04 +0800

    boost thread

--- src/boost_rar.cpp
+++ src/boost_rar.cpp
@@ -4,36 +4,60 @@
 #include <iostream>
 #include <sstream>
 #include <fstream>
+#include <string>
 #include <boost/iostreams/copy.hpp>
 #include <boost/iostreams/filter/gzip.hpp>
+using std::string;
+ void FastGzipString(const string& uncompressed, string* compressed) {
+  boost::iostreams::filtering_ostream out;
+  out.push(
+      boost::iostreams::gzip_compressor(boost::iostreams::zlib::best_speed));
+  out.push(boost::iostreams::back_inserter(*compressed));
+  boost::iostreams::write(out,
+                          reinterpret_cast<const char*>(uncompressed.data()),
+                          uncompressed.size());
+}
+
+ void FastGunzipString(const string& compressed, string* decompressed) {
+  boost::iostreams::filtering_ostream out;
+  out.push(boost::iostreams::gzip_decompressor());
+  out.push(boost::iostreams::back_inserter(*decompressed));
+  boost::iostreams::write(out, reinterpret_cast<const char*>(compressed.data()),
+                          compressed.size());
+}
+
+
 int main()
 {
     try{
-       // std::ifstream ifs("output", std::ios::binary);
+    
 
-        std::ifstream ifs;
+    std::ifstream ifs;
 
         ifs.open ("output", std::ifstream::in);
-
-        std::string dest;
+ std::vector<char>  temp, dest;
+     
         boost::iostreams::filtering_ostream out;
         out.push(boost::iostreams::gzip_compressor());
-       // out.push(boost::iostreams::file_sink("test1"));
-        out.push(boost::iostreams::back_inserter(dest));
-        //boost::iostreams::write(out, "hello", 5);
+        out.push(boost::iostreams::file_sink("testz.gz"));
+        //out.push(boost::iostreams::back_inserter(dest));
+        boost::iostreams::write(out, "3c1esba\n2222a\n4rratasfd", 20);
 
-        boost::iostreams::filtering_streambuf<boost::iostreams::output> compress_out;  
-        boost::iostreams::copy(ifs, out);
+        //boost::iostreams::filtering_streambuf<boost::iostreams::output> compress_out;  
+       // boost::iostreams::copy(ifs, out);
         //boost::iostreams::copy(std::stringstream("hello"), out);
         //std::cout << "dest:" << dest << std::endl;
-        boost::iostreams::filtering_istream in;
+        //out.clear();
+        boost::iostreams::close(out, std::ios_base::out);
+        boost::iostreams::filtering_ostream in;
+         std::stringstream ss_decomp;  
         in.push(boost::iostreams::gzip_decompressor());
-        //in.push(boost::iostreams::file_source("test.txt"));
-        std::stringstream ss(dest);
-        in.push(ss);
-        char text[100] = {0};
-        boost::iostreams::read(in, text, 10);
-        //boost::iostreams::copy(in, std::stringstream(text));
+        in.push(boost::iostreams::file_sink("testz.txt"));
+        char text[100];
+ boost::iostreams::write(in,boost::iostreams::file_source("testz.gz"));
+
+
+
         std::cout << "text:" << text << std::endl;
     }
     catch(std::exception& e)
